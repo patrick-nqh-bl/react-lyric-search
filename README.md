4b07577b0094c5fd1c3e71e521f3c8b30eb38bea
Patrick <patrick204nqh@gmail.com>
2020-05-12 14:13:51 +0700

    complete Lyrics page

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 1. Re-code by me
 
 - Source video:
+
   > https://www.youtube.com/watch?v=NDEt0KdDbhk
+
+  > https://www.youtube.com/watch?v=jiM0wqQZMWg
+
 - Musixmatch Developer Site:
   > https://developer.musixmatch.com/
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <div className='container'>
         <Switch>
           <Route exact path='/' component={Index} />
+          <Route exact path='/lyrics/track/:id' component={Lyrics} />
         </Switch>
       </div>
     </Router>
